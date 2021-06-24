0d949cc7cce5a2cfa554b1ea2bcc2abea587d717
agon <agostinonik@gmail.com>
2021-06-24 15:18:12 +0200

    dbabitazionenew

--- src/DbAbitazione.java
+++ src/DbAbitazione.java
@@ -3,36 +3,46 @@ import java.util.Map;
 import java.util.Set;
 
 public class DbAbitazione {
-    Map<Integer, Set<Prenotazione>> databaseAbitazione = new HashMap<Integer, Set<Prenotazione>>();
+    private Map<Integer, Set<Prenotazione>> databasePrenotazione = new HashMap<Integer, Set<Prenotazione>>();
+    private Map<Integer, Abitazione>  databaseAbitazioni = new HashMap<>();
 
     public DbAbitazione(Map<Integer, Set<Prenotazione>> databaseAbitazione) {
-        this.databaseAbitazione = databaseAbitazione;
+        this.databasePrenotazione = databaseAbitazione;
     }
 
     public Map<Integer, Set<Prenotazione>> getDatabaseAbitazione() {
-        return databaseAbitazione;
+        return databasePrenotazione;
     }
 
     public void setDatabaseAbitazione(Map<Integer, Set<Prenotazione>> databaseAbitazione) {
-        this.databaseAbitazione = databaseAbitazione;
+        this.databasePrenotazione = databaseAbitazione;
+    }
+
+    public Map<Integer, Abitazione> getDatabaseAbitazioni() {
+        return databaseAbitazioni;
+    }
+
+    public void setDatabaseAbitazioni(Map<Integer, Abitazione> databaseAbitazioni) {
+        this.databaseAbitazioni = databaseAbitazioni;
     }
 
     public int numeroPrenotazioni(Integer key){
-        return databaseAbitazione.get(key).size();
+        return databasePrenotazione.get(key).size();
     }
 
 
-    public int abitazioneGettonata(){
+    public Abitazione abitazioneGettonata(){
         int maxprenotazioni=0;
         int idgettonato=0;
-            for (Integer integer : databaseAbitazione.keySet()) {
+            for (Integer integer : databasePrenotazione.keySet()) {
                 if (numeroPrenotazioni(integer)>maxprenotazioni) {
                     maxprenotazioni = numeroPrenotazioni();
                     idgettonato = integer;
                 }
             }
-            return idgettonato;
+            return databaseAbitazioni.get(idgettonato);
         }
+
     }
 
 
