86d149db36cd149a0e720827141d261a3563b4cb
Ruben <laroccaruben1@icloud.com>
2021-06-24 12:13:19 +0200

    fix feedback

--- src/Feedback.java
+++ src/Feedback.java
@@ -32,7 +32,7 @@ public class Feedback {
     public int getValutazione() {
         return valutazione.getValue();
     }
-    //fine Costruttori
+    //fine get/setter
 
 
     public void setValutazione(Valutazione valutazione) {
