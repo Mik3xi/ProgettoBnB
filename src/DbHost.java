0c50bd4754506c703f1b81da8787a4f6131905dd
agon <agostinonik@gmail.com>
2021-06-25 10:14:26 +0200

    RicercaSuperOst

--- src/DbHost.java
+++ src/DbHost.java
@@ -1,8 +1,10 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class DbHost {
-    Map<Integer,Host> dbHost = new HashMap<>();
+    Map<Integer, Host> dbHost = new HashMap<>();
 
     public DbHost(Map<Integer, Host> dbHost) {
         this.dbHost = dbHost;
@@ -16,28 +18,35 @@ public class DbHost {
         this.dbHost = dbHost;
     }
 
-    public void aggiungihost(Host host){
-        if (getDbHost().containsKey(host.getId())){
+    public void aggiungihost(Host host) {
+        if (getDbHost().containsKey(host.getId())) {
             System.out.println("utente esistente");
-        }
-        else {
+        } else {
             getDbHost().put(host.getId(), host);
         }
     }
-    public Host ricercahost(Integer id){
-        Host host= null;
-        if(getDbHost().containsKey(id)){
-             host= getDbHost().get(id);
+
+    public Host ricercahost(Integer id) {
+        Host host = null;
+        if (getDbHost().containsKey(id)) {
+            host = getDbHost().get(id);
             System.out.println("Host trovato");
-        }
-        else
+        } else
             System.out.println("Host non trovato");
 
 
         return host;
-
     }
 
+    public Set<Host> ricercaSuperHost() {
+        Set<Host> superHostALL = new HashSet<>();
+        for (Integer key : getDbHost().keySet()) {
+            if(getDbHost().get(key).getTipo()==tipoHost.SUPER)
+                superHostALL.add(getDbHost().get(key));
+        }
+        return superHostALL;
+    }
+}
 
 
 }
